652d9a7e52f5ea4c4a040b2317eb48f9bde8283e
William Towe <willbur1984@gmail.com>
2017-09-30 05:35:56 -0400

    comments

--- KSOForm/KSOFormSection.h
+++ KSOForm/KSOFormSection.h
@@ -17,36 +17,114 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ String type that should be used for keys of a dictionary used to initialize the receiver.
+ */
 typedef NSString* KSOFormSectionKey NS_EXTENSIBLE_STRING_ENUM;
 
+/**
+ The KSOFormRow objects owned by the receiver. These can be either KSOFormRow objects or NSDictionary objects.
+ 
+ @see rows
+ */
 FOUNDATION_EXTERN KSOFormSectionKey const KSOFormSectionKeyRows;
+/**
+ The title of the section header in the UITableView displaying the form.
+ 
+ @see headerTitle
+ */
 FOUNDATION_EXTERN KSOFormSectionKey const KSOFormSectionKeyHeaderTitle;
+/**
+ The title of the section footer in the UITableView displaying the form.
+ 
+ @see footerTitle
+ */
 FOUNDATION_EXTERN KSOFormSectionKey const KSOFormSectionKeyFooterTitle;
+/**
+ The attributed title of the section header in the UITableView displaying the form.
+ 
+ @see headerAttributedTitle
+ */
 FOUNDATION_EXTERN KSOFormSectionKey const KSOFormSectionKeyHeaderAttributedTitle;
+/**
+ The attributed title of the section footer in the UITableView displaying the form.
+ 
+ @see footerAttributedTitle
+ */
 FOUNDATION_EXTERN KSOFormSectionKey const KSOFormSectionKeyFooterAttributedTitle;
+/**
+ The custom section header class that should be used for this section.
+ 
+ @see headerViewClass
+ */
 FOUNDATION_EXTERN KSOFormSectionKey const KSOFormSectionKeyHeaderViewClass;
+/**
+ The custom section footer class that should be used for this section.
+ 
+ @see footerViewClass
+ */
 FOUNDATION_EXTERN KSOFormSectionKey const KSOFormSectionKeyFooterViewClass;
 
 @class KSOFormRow,KSOFormModel;
 
+/**
+ KSOFormSection represents a single section in a KSOFormModel object. It is displayed using a single section in a UITableView. Changing any of its properties will automatically update its display within the UITableView displaying the form.
+ */
 @interface KSOFormSection : NSObject
 
+/**
+ Get the form that owns the receiver.
+ 
+ @see KSOFormModel
+ */
 @property (readonly,weak,nonatomic) KSOFormModel *model;
 
+/**
+ Get the identifier of the section. The identifier is created during initialization and cannot be changed.
+ */
 @property (readonly,copy,nonatomic) NSString *identifier;
 
+/**
+ Get whether the display of the receiver should use a section header.
+ */
 @property (readonly,nonatomic) BOOL wantsHeaderView;
+/**
+ Get whether the display of the receiver should use a section footer.
+ */
 @property (readonly,nonatomic) BOOL wantsFooterView;
 
+/**
+ Get the header title of the receiver.
+ */
 @property (copy,nonatomic,nullable) NSString *headerTitle;
+/**
+ Get the footer title of the receiver.
+ */
 @property (copy,nonatomic,nullable) NSString *footerTitle;
 
+/**
+ Get the header attributed title of the receiver. This value is preferred over headerTitle.
+ */
 @property (copy,nonatomic,nullable) NSAttributedString *headerAttributedTitle;
+/**
+ Get the footer attributed title of the receiver. This value is preferred over footerTitle.
+ */
 @property (copy,nonatomic,nullable) NSAttributedString *footerAttributedTitle;
 
+/**
+ Get the custom header view class used to display the receiver.
+ */
 @property (strong,nonatomic,nullable) Class headerViewClass;
+/**
+ Get the custom footer view class used to display the receiver.
+ */
 @property (strong,nonatomic,nullable) Class footerViewClass;
 
+/**
+ The KSOFormRow objects owned by the receiver. Accessing this property always returns a copy of the underlying NSMutableArray that contains the objects. To add/remove rows after creation, use the various public methods below.
+ 
+ @see KSOFormRow
+ */
 @property (readonly,copy,nonatomic) NSArray<KSOFormRow *> *rows;
 
 - (instancetype)initWithDictionary:(nullable NSDictionary<NSString *,id> *)dictionary model:(nullable KSOFormModel *)model NS_DESIGNATED_INITIALIZER;
