0d0f8e08ff9a0a942f13bad2b4db80b5e97f4d4d
William Towe <willbur1984@gmail.com>
2017-10-01 16:40:38 -0400

    adjust header visibility

--- KSOForm/KSOForm.h
+++ KSOForm/KSOForm.h
@@ -23,20 +23,23 @@ FOUNDATION_EXPORT const unsigned char KSOFormVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <KSOForm/PublicHeader.h>
 
+// defines
 #import <KSOForm/KSOFormModelDefines.h>
 #import <KSOForm/KSOFormSectionDefines.h>
 #import <KSOForm/KSOFormRowDefines.h>
 
+// protocols
 #import <KSOForm/KSOFormRowView.h>
 #import <KSOForm/KSOFormSectionView.h>
 #import <KSOForm/KSOFormRowValueDataSource.h>
 #import <KSOForm/KSOFormPickerViewRow.h>
 #import <KSOForm/KSOFormRowSegmentedItem.h>
 #import <KSOForm/KSOFormRowActionDelegate.h>
+#import <KSOForm/KSOFormThemeFirstResponderIndicatorView.h>
 
+// classes
 #import <KSOForm/KSOFormModel.h>
 #import <KSOForm/KSOFormSection.h>
 #import <KSOForm/KSOFormRow.h>
 #import <KSOForm/KSOFormTheme.h>
-
 #import <KSOForm/KSOFormTableViewController.h>
