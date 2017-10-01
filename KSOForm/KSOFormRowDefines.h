9bd5096a884ea6356da89b14a731b60e4b71d39a
William Towe <willbur1984@gmail.com>
2017-10-01 03:03:14 -0400

    comments

--- KSOForm/KSOFormRowDefines.h
+++ KSOForm/KSOFormRowDefines.h
@@ -144,7 +144,7 @@ typedef NSString* KSOFormRowKey NS_EXTENSIBLE_STRING_ENUM;
  */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyType;
 /**
- The value of the receiver. Should be appropriate for the type, unless a valueFormatter is provided.
+ The value of the receiver. Should be appropriate for the type, unless a valueFormatter is provided. Aligned to the trailing edge.
  
  @see [KSOFormRow value]
  */
@@ -187,69 +187,266 @@ UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyValueDidChangeBlock;
  */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyImage;
 /**
- The title of the receiver. Aligned to the leading edge.
+ The title of the receiver. Aligned to the leading edge, to the right of the image.
  
  @see [KSOFormRow title]
  */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyTitle;
 /**
- The subtitle of the receiver. Aligned to the leading edge.
+ The subtitle of the receiver. Aligned to the leading edge underneath the title.
  
  @see [KSOFormRow subtitle]
  */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeySubtitle;
+/**
+ The cell accessory type of the receiver. Must be one of the KSOFormRowCellAccessoryType values.
+ 
+ @see [KSOFormRow cellAccessoryType]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyCellAccessoryType;
-// text properties
+
+/**
+ The placeholder text of the text field or text view.
+ 
+ @see [KSOFormRow placeholder]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyPlaceholder;
+/**
+ The minimum number of lines for multiline text. The height of the text view will always be at least font.lineHeight * minLines.
+ 
+ @see [KSOFormRow minimumNumberOfLines]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyMinimumNumberOfLines;
+/**
+ The maximum number of lines for multiline text. The height of the text view can be at most font.lineHeight * maxLines before scrolling is required.
+ 
+ @see [KSOFormRow maximumNumberOfLines]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyMaximumNumberOfLines;
+/**
+ The object conforming to the KSOTextValidator protocol to handle text validation for the receiver.
+ 
+ @see [KSOFormRow textValidator]
+ @see KSOTextValidator
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyTextValidator;
+/**
+ The object conforming to the KSOTextFormatter protocol to handle text formatting for the receiver. NSFormatter conforms to KSOTextFormatter.
+ 
+ @see [KSOFormRow textFormatter]
+ @see KSOTextFormatter
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyTextFormatter;
-// UITextInputTraits
+
+/**
+ The autocapitalization type.
+ 
+ @see [UITextInputTraits autocapitalizationType]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyAutocapitalizationType;
+/**
+ The autocorrection type.
+ 
+ @see [UITextInputTraits autocorrectionType]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyAutocorrectionType;
+/**
+ The spell checking type.
+ 
+ @see [UITextInputTraits spellCheckingType]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeySpellCheckingType;
+/**
+ The smart quotes type.
+ 
+ @see [UITextInputTraits smartQuotesType]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeySmartQuotesType NS_AVAILABLE_IOS(11_0);
+/**
+ The smart dashes type.
+ 
+ @see [UITextInputTraits smartDashesType]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeySmartDashesType NS_AVAILABLE_IOS(11_0);
+/**
+ The smart insert delete type.
+ 
+ @see [UITextInputTraits smartInsertDeleteType]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeySmartInsertDeleteType NS_AVAILABLE_IOS(11_0);
+/**
+ The keyboard type.
+ 
+ @see [UITextInputTraits keyboardType]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyKeyboardType;
+/**
+ The keyboard appearance. This will override the keyboard appearance from [KSOTheme keyboardAppearance].
+ 
+ @see [UITextInputTraits keyboardAppearance]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyKeyboardAppearance;
+/**
+ The return key type.
+ 
+ @see [UITextInputTraits returnKeyType]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyReturnKeyType;
+/**
+ Whether the return is enabled automatically.
+ 
+ @see [UITextInputTraits enablesReturnKeyAutomatically]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyEnablesReturnKeyAutomatically;
+/**
+ Whether text entry is secure (e.g. password entry).
+ 
+ @see [UITextInputTraits secureTextEntry]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeySecureTextEntry;
+/**
+ The text content type.
+ 
+ @see [UITextInputTraits textContentType]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyTextContentType;
-// UIPickerView
+
+/**
+ The picker view columns and rows. Use this to specify multiple columns.
+ 
+ @see [KSOFormRow pickerViewColumnsAndRows]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyPickerViewColumnsAndRows;
+/**
+ The picker view rows. Use this to specify a single column.
+ 
+ @see [KSOFormRow pickerViewRows]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyPickerViewRows;
+/**
+ The picker view selected components join string.
+ 
+ @see [KDIPickerViewButton selectedComponentsJoinString]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyPickerViewSelectedComponentsJoinString;
-// UIDatePicker
+
+/**
+ The date picker mode.
+ 
+ @see [UIDatePicker datePickerMode]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyDatePickerMode;
+/**
+ The date picker minimum date.
+ 
+ @see [UIDatePicker minimumDate]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyDatePickerMinimumDate;
+/**
+ The date picker maximum date.
+ 
+ @see [UIDatePicker maximumDate]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyDatePickerMaximumDate;
+/**
+ The date formatter used to format the date picker date for display.
+ 
+ @see [KDIDatePickerButton dateFormatter]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyDatePickerDateFormatter;
-// UIStepper & UISlider
+
+/**
+ The minimum value.
+ 
+ @see [UISlider minimumValue]
+ @see [UIStepper minimumValue]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyMinimumValue;
+/**
+ The maximum value.
+ 
+ @see [UISlider maximumValue]
+ @see [UIStepper maximumValue]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyMaximumValue;
-// UIStepper
+
+/**
+ The stepper step value.
+ 
+ @see [UIStepper stepValue]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyStepperStepValue;
-// UISlider
+
+/**
+ The slider minimum value image.
+ 
+ @see [UISlider minimumValueImage]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeySliderMinimumValueImage;
+/**
+ The slider maximum value image.
+ 
+ @see [UISlider maximumValueImage]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeySliderMaximumValueImage;
-// UIControl (e.g. UIButton)
+
+/**
+ The block that should be invoked when the control is tapped (e.g. UIButton tap).
+ 
+ @see KDIUIControlBlock
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyControlBlock;
-// UISegmentedControl
+
+/**
+ The segmented control items. Must conform to KSOFormRowSegmentedItem protocol.
+ 
+ @see KSOFormRowSegmentedItem
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeySegmentedItems;
-// present/push support
+
+/**
+ The row action.
+ 
+ @see [KSOFormRow action]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyAction;
+/**
+ The action delegate.
+ 
+ @see KSOFormRowActionDelegate
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyActionDelegate;
+/**
+ The KSOFormModel object to assign to the action view controller.
+ 
+ @see KSOFormModel
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyActionModel;
+/**
+ The action view controller class.
+ 
+ @see [KSOFormRow actionViewControllerClass]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyActionViewControllerClass;
-// custom cell support
+
+/**
+ The table view cell class.
+ 
+ @see [KSOFormRow cellClass]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyCellClass;
-UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyCellIdentifier;
-UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyHeaderFooterViewIdentifier;
-// UIAccessibility
+
+/**
+ The accessibility label for the image.
+ 
+ @see [UIAccessibility accessibilityLabel]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyImageAccessibilityLabel;
+/**
+ The accessibility hint for the button.
+ 
+ @see [UIAccessibility accessibilityHint]
+ */
 UIKIT_EXTERN KSOFormRowKey const KSOFormRowKeyButtonAccessibilityHint;
 
 NS_ASSUME_NONNULL_END
